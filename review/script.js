9137aac4449c89b00b14bb6c5f030f4444b78f96
Ching <89380886+Chingsan53@users.noreply.github.com>
2023-10-01 14:51:49 -0700

    update guess my number

---
+++ review/script.js
@@ -0,0 +1,16 @@
+const square = function(x) {
+    return x * x;
+};
+
+console.log(square(12));
+
+
+const power = function(base, exponent) {
+    let result = 1;
+    for (let count = 0; count < exponent; count++) {
+        result *= base;
+    }
+    return result;
+};
+
+console.log(power(2, 4));
