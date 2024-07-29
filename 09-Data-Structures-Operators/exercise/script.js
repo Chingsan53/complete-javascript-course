40fa63eb34f156b42a1cd93ce4f259a26f84f19f
Ching <89380886+Chingsan53@users.noreply.github.com>
2024-07-28 22:00:59 -0700

    did 2 exercises

--- 09-Data-Structures-Operators/exercise/script.js
+++ 09-Data-Structures-Operators/exercise/script.js
@@ -181,3 +181,21 @@ const books = [
       highlighted: true
     }
   ];
+
+  // 1.1 Destructure the books array into two variables called firstBook and secondBook.
+  const [firstBook, secondBook] = books;
+  console.log(firstBook, secondBook);
+
+
+// 1.2 Destructure the books array into a variable called thirdBook. You must skip the first two books.
+  const [, , thirdBook] = books;
+  console.log(thirdBook);
+
+  /*
+    1.3 Below is the nested ratings array that contains two other arrays. Destructure the nested ratings arrays into two variables called rating and ratingsCount. In the result of your destructuring, the ratings variable should store a number 4.19, and the ratingsCount variable should store a number 144584.
+  */
+
+
+  /*
+    Below is the ratingStars array. Destructure it into three variables called fiveStarRatings, oneStarRatings and threeStarRatings. Assign the threeStarRatings variable with a default value of 0.
+  */
