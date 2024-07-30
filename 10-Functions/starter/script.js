33161e2df56f02bebb23d4f45ae41b2fe85368c8
Ching <89380886+Chingsan53@users.noreply.github.com>
2024-07-30 15:55:22 -0700

    complete section 9

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,1 +1,26 @@
 'use strict';
+
+const bookings = [];
+
+const createBooking = function (
+  flightNum,
+  numPassengers = 1,
+  price = 199 * numPassengers
+) {
+  // ES5
+  //   numPassengers = numPassengers || 1;
+  //   price = price || 199;
+
+  const booking = {
+    flightNum,
+    numPassengers,
+    price,
+  };
+  console.log(booking);
+  bookings.push(booking);
+};
+
+createBooking('LH123', 1, 299);
+createBooking('LH123', 2, 599);
+createBooking('LH125', 2);
+console.log(bookings);
