9e7c4613560e376acdb8d789c0ff72e4ced8c46e
Chingsan53 <89380886+Chingsan53@users.noreply.github.com>
2024-08-01 01:48:56 -0700

    added checkPassport in function

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -24,3 +24,34 @@ createBooking('LH123', 1, 299);
 createBooking('LH123', 2, 599);
 createBooking('LH125', 2);
 console.log(bookings);
+
+const flight = 'LH234';
+const chingsan = {
+  name: 'Chingsan Ly',
+  passport: 24738388282,
+};
+
+const checkIn = function (flightNum, passenger) {
+  flightNum = 'Lh999';
+  passenger.name = 'Mr. ' + passenger.name;
+
+  if (passenger.passport === 24738388282) {
+    alert('Check in');
+  } else {
+    alert('Wrong Passport');
+  }
+};
+
+// checkIn(flight, chingsan);
+// console.log(flight);
+// console.log(chingsan);
+
+// const flightNum = flight;
+// const passenger = chingsan;
+
+const newPassport = function (person) {
+  person.passport = Math.trunc(Math.random() * 1000000000);
+};
+
+newPassport(chingsan);
+checkIn(flight, chingsan);
