27aba932b9241bf37918052fd8dc21eb9b22123c
Chingsan53 <cly53@csu.fullerton.edu>
2024-08-01 12:31:41 -0700

    added functions and challenges

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -25,6 +25,7 @@ createBooking('LH123', 2, 599);
 createBooking('LH125', 2);
 console.log(bookings);
 
+/*
 const flight = 'LH234';
 const chingsan = {
   name: 'Chingsan Ly',
@@ -55,3 +56,146 @@ const newPassport = function (person) {
 
 newPassport(chingsan);
 checkIn(flight, chingsan);
+*/
+
+// Functions Accepting Callback Functions
+const oneWord = function (str) {
+  return str.replace(/ /g, '').toLowerCase();
+};
+
+const upperFirstWord = function (str) {
+  const [first, ...others] = str.split(' ');
+  return [first.toUpperCase(), ...others].join(' ');
+};
+
+// Higher-order function
+const transformer = function (str, fn) {
+  console.log(`Original string: ${str}`);
+  console.log(`Transformed string: ${fn(str)}`);
+
+  console.log(`Transformed by: ${fn.name}`);
+};
+transformer('JavaScript is the best!', upperFirstWord);
+transformer('JavaScript is the best!', oneWord);
+
+// JS uses callbacks all the time
+const high5 = function () {
+  // console.log('👋');
+};
+document.body.addEventListener('click', high5);
+// ['Jonas', 'Martha', 'Adam'].forEach(high5);
+
+// Functions Returning Functions
+const greet = function (greeting) {
+  return function (name) {
+    console.log(`${greeting} ${name}`);
+  };
+};
+
+const greeterHey = greet('Hey');
+greeterHey('Jonas');
+greeterHey('Steven');
+
+greet('Hello')('Jonas');
+
+// Challenge
+const greetArr = greeting => name => console.log(`${greeting} ${name}`);
+
+greetArr('Hi')('Chingsan');
+
+// The call and apply Methods
+const asiana = {
+  airline: 'Asiana',
+  iataCode: 'LH',
+  bookings: [],
+  book(flightNum, name) {
+    console.log(
+      `${name} booked a seat on ${this.airline} flight ${this.iataCode}${flightNum}`
+    );
+    this.bookings.push({ flight: `${this.iataCode}${flightNum}`, name });
+  },
+};
+
+asiana.book(239, 'Chingsan');
+asiana.book(899, 'Chingsien');
+console.log(asiana);
+
+const delta = {
+  airline: 'Delta',
+  iataCode: 'DT',
+  bookings: [],
+};
+
+const book = asiana.book;
+// console.log(typeof book);
+// Does not work
+// book(23, 'Sarah Williams');
+
+book.call(delta, 23, 'Sarah Williams');
+console.log(delta);
+
+book.call(asiana, 239, 'Chingsan Ly');
+console.log(asiana);
+console.log(asiana.bookings);
+
+const korean = {
+  airline: 'Korean Air',
+  iataCode: 'KR',
+  bookings: [],
+};
+
+book.call(korean, 583, 'Mary Cooper');
+console.log(korean);
+
+// Apply method
+const flightData = [583, 'George Cooper'];
+book.apply(korean, flightData);
+console.log(korean);
+
+book.call(korean, ...flightData);
+
+// Bind Methods
+const bookDT = book.bind(delta);
+const bookAN = book.bind(asiana);
+const bookKR = book.bind(korean);
+bookDT(23, 'Steven Liu');
+bookAN(43, 'Mario Liu');
+
+const bookDT34 = book.bind(delta, 34);
+bookDT34('David Hackathon');
+bookDT34('Joana Lee');
+
+// With Event Listeners
+asiana.planes = 300;
+asiana.buyPlane = function () {
+  console.log(this);
+  this.planes++;
+  console.log(this.planes);
+};
+asiana.buyPlane();
+
+document
+  .querySelector('.buy')
+  .addEventListener('click', asiana.buyPlane.bind(asiana));
+
+// Partial application
+
+const addTax = (rate, value) => value + value * rate;
+console.log(addTax(0.1, 200));
+
+const addVAT = addTax.bind(null, 0.23);
+// addVAT = value => value + value * 0.23
+
+console.log(addVAT(100));
+console.log(addVAT(23));
+
+// Similarly =>
+const addTaxRate = function (rate) {
+  return function (value) {
+    return value + value * rate;
+  };
+};
+
+const addVAT2 = addTaxRate(0.23);
+console.log(addVAT2(100));
+console.log(addVAT2(23));
