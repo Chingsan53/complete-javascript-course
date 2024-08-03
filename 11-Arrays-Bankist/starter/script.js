a7bea4090f6a23b7c2193504427eb3bbfd38cae7
Chingsan53 <89380886+Chingsan53@users.noreply.github.com>
2024-08-02 17:54:28 -0700

    added reduce method

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -65,6 +65,13 @@ const inputClosePin = document.querySelector('.form__input--pin');
 /////////////////////////////////////////////////
 // LECTURES
 
+const displayMovements = function (movements) {
+  movements.forEach(function (mov, i) {
+    const html = ``;
+  });
+};
+displayMovements(account1.movements);
+
 // const currencies = new Map([
 //   ['USD', 'United States dollar'],
 //   ['EUR', 'Euro'],
@@ -163,3 +170,108 @@ console.log(currenciesUnique);
 currenciesUnique.forEach(function (value, _, map) {
   console.log(`${value}: ${value}`);
 });
+
+const euroToUsd = 1.1;
+const movementsUSD = movements.map(function (mov) {
+  return mov * euroToUsd;
+});
+console.log(movements);
+console.log(movementsUSD);
+
+const movementsUSDfor = [];
+
+for (const mov of movements) {
+  movementsUSDfor.push(mov * euroToUsd);
+}
+
+console.log(movementsUSDfor);
+
+const movementsDescription = movements.map(
+  (mov, i) =>
+    `Movement ${i + 1}: You ${mov > 0 ? 'deposited' : 'withdrew'} ${Math.abs(
+      mov
+    )}`
+  // if (mov > 0) {
+  //   return `Movement ${i + 1}: You deposited ${mov}`;
+  // } else {
+  //   return `Movement ${i + 1}: You withdraw ${Math.abs(mov)}`;
+  // }
+);
+console.log(movementsDescription);
+
+// Create user name
+//const user = 'Steven Thomas Williams'; // stw
+const createUsername = function (accs) {
+  accs.forEach(function (acc) {
+    acc.username = acc.owner
+      .toLowerCase()
+      .split(' ')
+      .map(e => e[0])
+      .join('');
+  });
+};
+
+createUsername(accounts);
+console.log(accounts);
+
+// Filter
+const deposits = movements.filter(function (mov) {
+  return mov > 0;
+});
+console.log(movements);
+console.log(deposits);
+
+const depositsFor = [];
+for (const mov of movements) if (mov > 0) depositsFor.push(mov);
+console.log(depositsFor);
+
+// const withdrawals = [];
+// for (const mov of movements) if (mov < 0) withdrawals.push(mov);
+// console.log(withdrawals);
+
+const withdrawals = movements.filter(mov => mov < 0);
+console.log(withdrawals);
+
+console.log(movements);
+
+// accumulator -> SNOWBALL
+const balance = movements.reduce(function (acc, cur, i, arr) {
+  console.log(`Iteration ${i}: ${acc}`);
+  return acc + cur;
+}, 0);
+console.log(balance);
+
+// Similarly
+let balance2 = 0;
+// let sum = 0;
+for (const mov of movements) balance2 += mov;
+console.log(balance2);
+
+// Arrow function similar to SNOWBALL above
+const balance3 = movements.reduce((acc, cur, i, arr) => acc + cur);
+console.log(balance3);
+
+const calcDisplayBalance = function (movements) {
+  const balance = movements.reduce((acc, mov) => acc + mov, 0);
+  labelBalance.textContent = `${balance} EUR`;
+  console.log(`Display Balance: ${balance}`);
+};
+
+calcDisplayBalance(account1.movements);
+calcDisplayBalance(account2.movements);
+calcDisplayBalance(account3.movements);
+
+// Maximum value
+const max = movements.reduce((acc, mov) => {
+  if (acc > mov) return acc;
+  else return mov;
+}, movements[0]);
+console.log(`The maximum deposit value: ${max}`);
+
+// Minimum value
+const min = movements.reduce((acc, mov) => {
+  if (acc < mov) return acc;
+  else return mov;
+});
+
+console.log(`The minimum deposit value: ${min}`);
